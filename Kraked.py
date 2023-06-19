06f3078682b3b14c3fa560d969cf9f96d51b1729
OussamaZermouq <oussamazermouq2@gmail.com>
2023-06-19 04:17:01 +0100

    Switched from predb.net API to srrDB

--- Kraked.py
+++ Kraked.py
@@ -1,12 +1,15 @@
 import requests
 
 
-url ='https://api.predb.net/?q=spiderman&genre=GAMES'
+
+search_query = 'f1.2020'
+url =f'https://api.srrdb.com/v1/search/{search_query}/category:pc'
 
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    print(data["data"]["release"])
+    for releases in data["results"]:
+        print(releases["release"])
 else:
     print(f'Error {response.status_code}')
