ff697a477a1b812eb0152a407e7e3e1b1d1e91b1
OussamaZermouq <oussamazermouq2@gmail.com>
2023-06-19 02:53:57 +0100

    A simple API request

--- Kraked.py
+++ Kraked.py
@@ -1,1 +1,12 @@
-print ('hello world')
+import requests
+
+
+url ='https://api.predb.net/?q=spiderman&genre=GAMES'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data["data"]["release"])
+else:
+    print(f'Error {response.status_code}')
