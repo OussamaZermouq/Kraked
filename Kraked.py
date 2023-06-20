2e09f1e87fb9152c409854889d76bbca69b304df
OussamaZermouq <oussamazermouq2@gmail.com>
2023-06-20 02:33:18 +0100

    In Beta version the base functionality works

--- Kraked.py
+++ Kraked.py
@@ -10,17 +10,23 @@ client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='/', intents=intents)
 TOKEN= 'MTEyMDM0NjcyMzY2ODIwNTU4OA.GCt14g.4ySAAK5s07-PXBu_7PjbqOfc8vzOU6JHAjoKus'
 query=''
-output='\n'
-
 
 def send_req(search_query):
     search_query = search_query.replace(' ','.')
     url =f'https://api.srrdb.com/v1/search/{search_query}/category:pc'
     response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()    
+        output = ''
+        data = response.json()
+        #check if a release is cracked or not
+        if data["resultsCount"]>=1:
+            global is_cracked
+            is_cracked = True
+        elif data["resultsCount"]==0:
+            
+            is_cracked = False
         for releases in data["results"]:
-            output =+ releases+'\n'
+            output = output+releases["release"]+'\n'
         return output
     else:
         print(f'Error {response.status_code}')
@@ -29,7 +35,14 @@ def send_req(search_query):
 @bot.command()
 async def status(message, *args):
     query = '.'.join(args)
-    embed = discord.Embed(title="Status", description=send_req(query), color=0x00ff00)
+    result = send_req(query)
+    if is_cracked ==True:
+        title = "Stauts: Cracked ✅ \nReleases:"
+        color =0x00ff00
+    else:
+        title ="Status : Not Cracked! 😔"
+        color=0xff0000
+    embed = discord.Embed(title=title, description = result , color=color)
     await message.send(embed=embed)
 
 
